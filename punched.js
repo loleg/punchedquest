dd93467b5c16e6ec7c732dd8cc4309fb6f064324
Oleg Lavrovsky <oleg@utou.ch>
2015-07-29 16:18:18 +0200

    Initial prototype

---
+++ punched.js
@@ -0,0 +1,74 @@
+// based on the 'quick' sample
+
+RPGJS.Materials = {
+	"characters": {
+		"1": "event1.png"
+	},
+	"tilesets": {
+		"1": "tileset.png"
+	}
+};
+
+RPGJS.Database = {
+	"actors": {
+		"1": {
+			"graphic": "1"
+		}
+	},
+	"tilesets": {
+		"1": {
+			"graphic": "1"
+		}
+	},
+	"map_infos": {
+		"1": {
+			"tileset_id": "1"
+		}
+	}
+};
+
+RPGJS.defines({
+	canvas: "canvas",
+	autoload: false,
+	scene_path: "RPG-JS/"
+}).ready(function() {
+
+	RPGJS.Player.init({
+		actor: 1,
+		start: {x: 10, y: 10, id: 1}
+	});
+
+	RPGJS.Scene.map();
+});
+
+var $punchcard = $('#punchcard');
+
+for (var i = 0; i < 10; i++) {
+	var prow = $punchcard
+			.append('<div class="punch row"></div>').find('.row:last');
+	for (var j = 0; j < 4; j++) {
+		prow.append('<span><input type="checkbox"></span>');
+	}
+}
+
+$('#punchit').click(function() {
+	var seq = [];
+	$('.punch.row:has(input:checked)', $punchcard).each(function() {
+		var step = [];
+		$('input', this).each(function() {
+			step.push(this.checked);
+		});
+		seq.push(step);
+	}).promise().done(function() {
+		var route = [];
+		seq.forEach(function(step) {
+			if (step[0]) route = route.concat(["turn_left", "wait_10", "left"]);
+			if (step[1]) route = route.concat(["turn_right", "wait_10", "right"]);
+			if (step[2]) route = route.concat(["turn_up", "wait_10", "up"]);
+			if (step[3]) route = route.concat(["turn_bottom", "wait_10", "bottom"]);
+			route.push("wait_40");
+		});
+		console.log(route);
+		RPGJS.Player.moveRoute(route);
+	});
+});
