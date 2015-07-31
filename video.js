702d89c7bdd3cd97f02487dee8176b3141e0e732
Oleg Lavrovsky <oleg@utou.ch>
2015-07-31 17:33:39 +0200

    Blob post to service

--- video.js
+++ video.js
@@ -2,6 +2,8 @@ $('#scanit').click(initVideoCapture);
 
 function initVideoCapture() {
 
+var haveCapture = false;
+
 var video = document.getElementById('webcam');
 var canvas = document.getElementById('capture');
 var preview = document.getElementById('preview');
@@ -24,6 +26,8 @@ function snapshot() {
             // Hide the other image
             preview.style.display = "";
             video.style.display = "none";
+
+            haveCapture = true;
           });
    });
  }
@@ -33,6 +37,38 @@ function resett() {
   preview.style.display = "none";
   video.style.display = "";
 }
+
+
+function dataURItoBlob(dataURI) {
+    var binary = atob(dataURI.split(',')[1]);
+    var array = [];
+    for(var i = 0; i < binary.length; i++) {
+        array.push(binary.charCodeAt(i));
+    }
+    return new Blob([new Uint8Array(array)], {type: 'image/jpeg'});
+}
+
+
+$('#sendcard').click(function() {
+  if (!haveCapture) {
+    alert('Please tap on the preview to freeze the frame');
+    return false;
+  }
+
+  var data = new FormData();
+  data.append('file', dataURItoBlob(preview.src));
+
+  $.ajax('/instructions', {
+    method: 'POST',
+    cache: false,
+    contentType: false,
+    processData: false,
+    data: data,
+    success: function(response) {
+      console.log(response);
+    }
+  });
+});
 
 video.addEventListener('click', snapshot, false);
 preview.addEventListener('click', resett, false);
